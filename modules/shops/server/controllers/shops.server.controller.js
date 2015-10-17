ccdbb7f9f44fa620a702f8a5f4fe4c7eb2314a4b
Vee Trairattanapa <happierv@gmail.com>
2015-10-17 20:06:17 +0700

    Shortcut the category page

--- modules/shops/server/controllers/shops.server.controller.js
+++ modules/shops/server/controllers/shops.server.controller.js
@@ -31,7 +31,10 @@ exports.list = function (req, res) {
   });
 };
 exports.list_by_category = function (req, res, next, category) {
-  Shop.find({is_shop: 1, category: category}).limit(10).exec(function (err, shops) {
+  Shop.find({
+    is_shop: 1, 
+    bio: new RegExp(category, 'i')
+  }).limit(10).exec(function (err, shops) {
   // Shop.find().sort('-created').limit(10).populate('user', 'displayName').exec(function (err, shops) {
     if (err) {
       return res.status(400).send({
